2779252877e40c68a79d1f7903bcc09b26df7c7b
Pawel Staniul <pstaniul@gmail.com>
2023-04-15 13:54:46 +0200

    utworzono klasę Product

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace MiodOdStaniula.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        public int Priority { get; set; }
+
+        public string Category { get; set; }
+
+        public string Name { get; set; }
+        
+        public int Price { get; set; }
+
+        public string Weight { get; set; }
+        
+        public string Description { get; set; }
+
+        public int AmountAvailable { get; set; }
+
+
+
+    }
+}
