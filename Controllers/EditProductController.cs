095752db382f69ad2870ab03fbc8a39557c6f6cb
Paweł Staniul <pstaniul@gmail.com>
2023-07-30 14:49:50 +0200

    Ujednolicono formularze tworzenia i edycji produktu, zaimplementowano mozliwość dodawania zdjęć.

--- Controllers/EditProductController.cs
+++ Controllers/EditProductController.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using MiodOdStaniula;
+using Microsoft.Extensions.Logging;
 using MiodOdStaniula.Models;
-using MiodOdStaniula.Services;
 using MiodOdStaniula.Services.Interfaces;
 
 namespace MiodOdStaniula.Controllers
@@ -17,12 +10,15 @@ namespace MiodOdStaniula.Controllers
     public class EditProductController : Controller
     {
         private readonly IEditProductService _editProductService;
+        private readonly IFileUploadService _fileUploadService;
         private readonly IWarehouseService _warehouseService;
         private readonly ILogger<IEditProductService> _logger;
 
-        public EditProductController(IEditProductService editProductService, ILogger<IEditProductService> logger, IWarehouseService warehouseService)
+        public EditProductController(IEditProductService editProductService, IFileUploadService fileUploadService,
+        ILogger<IEditProductService> logger, IWarehouseService warehouseService)
         {
             _editProductService = editProductService;
+            _fileUploadService = fileUploadService;
             _logger = logger;
             _warehouseService = warehouseService;
         }
@@ -30,21 +26,37 @@ namespace MiodOdStaniula.Controllers
         [HttpGet]
         public async Task<IActionResult> Edit(int ProductId)
         {
-            var product = await _warehouseService.GetProductAsync(ProductId);
-            if(product == null)
+            Product? product = await _warehouseService.GetProductAsync(ProductId)!;
+
+            if (product == null)
             {
                 return View("_NotFound");
             }
-            return View(product);
+            else
+            {
+                ProductViewModel productViewModel = new()
+                {
+                    Name = product.Name,
+                    Priority = product.Priority,
+                    Description = product.Description,
+                    Price = product.Price,
+                    Weight = product.Weight,
+                    AmountAvailable = product.AmountAvailable,
+                    CategoryId = product.CategoryId,
+                    PhotoUrlAddress = product.PhotoUrlAddress
+                };
+                return View(productViewModel);
+            }
         }
 
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int ProductId,
-        [Bind("ProductId,Priority,CategoryId,Name,Price,Weight,Description,AmountAvailable,PhotoUrlAddress")] Product product)
+        public async Task<IActionResult> Edit(int ProductId, ProductViewModel model)
         {
-            if (ProductId != product.ProductId)
+            Product? product = await _warehouseService.GetProductAsync(ProductId)!;
+
+            if (product == null || ProductId != product.ProductId)
             {
                 return View("_NotFound");
             }
@@ -54,16 +66,42 @@ namespace MiodOdStaniula.Controllers
                 return View(product);
             }
 
-            var result = await _editProductService.UpdateProductAsync(product);
+            string? photoUrlAddress = product.PhotoUrlAddress;
 
-            if (result.IsSuccess)
+            // If a new photo was uploaded.
+            if (model.ProductImage != null)
+            {
+                // Use FileUploadService to upload the file.
+                var uploadResult = await _fileUploadService.UploadFileAsync(model.ProductImage);
+                if (uploadResult != null)
+                {
+                    photoUrlAddress = uploadResult;
+                }
+            }
+
+            product = new Product
+            {
+                ProductId = model.ProductId,
+                Priority = model.Priority,
+                CategoryId = model.CategoryId,
+                Name = model.Name,
+                Price = model.Price,
+                Weight = model.Weight,
+                Description = model.Description,
+                AmountAvailable = model.AmountAvailable,
+                PhotoUrlAddress = photoUrlAddress
+            };
+
+            var (IsSuccess, Error) = await _editProductService.UpdateProductAsync(product);
+
+            if (IsSuccess)
             {
                 return RedirectToAction("Index", "Warehouse");
             }
 
-            if (result.Error != null)
+            if (Error != null)
             {
-                return HandleUpdateError(result.Error, product);
+                return HandleUpdateError(Error, product);
             }
             return View(product);
         }
@@ -71,7 +109,7 @@ namespace MiodOdStaniula.Controllers
 
         private IActionResult HandleUpdateError(Exception ex, Product product)
         {
-            _logger.LogError(ex, $"Wystąpił błąd podczas aktualizacji danych produktu o ID: {product.ProductId}", product.ProductId);
+            _logger.LogError(ex, "Wystąpił błąd podczas aktualizacji produktu. Spróbuj ponownie później.");
             ModelState.AddModelError(string.Empty, "Wystąpił błąd podczas aktualizacji produktu. Spróbuj ponownie później.");
             return View(product);
         }
